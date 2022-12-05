3ecd1a2595984e9b5f7be5dcb26f48b40b2e1e13
sdmantha <sdangelcake@gmail.com>
2022-12-05 15:45:04 -0500

    wrote the info thats going on in js, still need to complete a readme

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
+//create variables and consts
 const cards = document.querySelectorAll('.card');
 const playerLifeCount= document.querySelector("span")
 let playerLives= 20;
 
+//pop up on the screen
 playerLifeCount.textContent= playerLives;
 
-
+//create more varialbes
   let hasFlippedCard = false;
   let firstCard, secondCard;
   let lockBoard=false;
+  let allCardsFlipped=true;
 
 
+// 'this' represents that the card was flipped 
+//'hasflippedcard' is true is no card is flipped
+//flipped card is set to the clicked card
   function flipCard() {
     if(lockBoard) return;
     if (this === firstCard) return;
@@ -19,10 +25,12 @@ playerLifeCount.textContent= playerLives;
       firstCard = this;
      return;
    }
+   //calling the function
    secondCard = this;
    checkForMatch();
  }
-
+//ternary function
+//added data atrributes in the html to each picture so that I can match them up in JS and create a dataset. A match is the first and second data set and if they dont match then the cards got back to "unflipcards"
  function checkForMatch() {
     let isMatch = firstCard.dataset.match === secondCard.dataset.match;
  isMatch ? disableCards() : unflipCards();
@@ -32,16 +40,19 @@ playerLifeCount.textContent= playerLives;
    firstCard.removeEventListener('click', flipCard);
    secondCard.removeEventListener('click', flipCard);
 
+//called the reset func
    resetBoard();
  }
-
+//lockboard will prevent any card flipping before the cards are hidden or match
  function unflipCards() {
     lockBoard =true;
+    //time out lets the card flip back over at a certain speed in ms
    setTimeout(() => {
      firstCard.classList.remove('flip');
      secondCard.classList.remove('flip');
      resetBoard();
    }, 975);
+   //subtract one playerlives(life)
    playerLives--;
    if (playerLives=== 0){
      alert("Game Over!");
@@ -49,7 +60,13 @@ playerLifeCount.textContent= playerLives;
    playerLifeCount.textContent= playerLives;   
  }
 
+ function winBoard(){
+  if (firstCard.dataset.framework === secondCard.dataset.framework){
+    container.style.display="none"
+  }
+ }
 
+//if the hasFlippedCard(not flipped) and lockboard(no cards ) are false and the first and second data match are null then the board can be reset
  function resetBoard() {
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
@@ -57,15 +74,15 @@ playerLifeCount.textContent= playerLives;
     
   }
 
-
+//shuffle cards
  (function shuffle() {
    cards.forEach(card => {
      let ramdomPos = Math.floor(Math.random() * 12);
      card.style.order = ramdomPos;
    });
  })();
-
+//called the function
   cards.forEach(card => card.addEventListener('click', flipCard));
 
   //add module for game over
-  //reset game state, maybe put it insid eof the module 
+  //reset game state, maybe put it inside of the module 
